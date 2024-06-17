166033d02219fd0ce47361c95e66d61edf3bf689
Kartikey <singhkartikey008@gmail.com>
2024-06-17 21:06:13 +0530

    dashboard Creation

---
+++ src/router.tsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import LoginPage from '@/pages/LoginPage.tsx';
+import HomePage from '@/pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardLayout from './layouts/DashboardLayout.tsx';
+import {BooksPage} from './pages/BooksPage.tsx';
+import AuthLayout from './layouts/AuthLayout.tsx';
+import CreateBook from './pages/CreateBook.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Navigate to="/dashboard/home" />,
+    },
+    {
+        path: '/dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+            {
+                path: 'books',
+                element: <BooksPage />,
+            },
+            {
+                path: 'books/create',
+                element: <CreateBook />,
+            },
+        ],
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <LoginPage />,
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />,
+            },
+        ],
+    },
+]);
+
+export default router;
